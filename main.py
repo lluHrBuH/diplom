4e99869ece52abfec3a1e55039c0fe77acd4ddd5
vlad <lluhacolada@gmail.com>
2018-07-12 03:08:35 +0300

    add time plot

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 from searcher import Searcher
-from plot import plotAllMetric
+from plot import plotAllMetric, plotAllFreq
 
 
 
-X_train = np.load('X_train.npy')
-y_train = np.load('y_train.npy')
-X_test = np.load('X_test.npy')
-y_test = np.load('y_test.npy')
+#X_train = np.load('X_train.npy')
+#y_train = np.load('y_train.npy')
+#X_test = np.load('X_test.npy')
+#y_test = np.load('y_test.npy')
 
 
-#X = np.load('./data_vggface/embeddings.npy')
-#y = np.load('./data_vggface/labels.npy')
+X = np.load('./data_vggface/embeddings.npy')
+y = np.load('./data_vggface/labels.npy')
 
-#X_train, X_test, y_train, y_test = train_test_split(
-#     X, y, test_size=0.33, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+     X, y, test_size=0.33, random_state=42)
 
 plotAllMetric(X_train, y_train, X_test, y_test)
+plotAllFreq(X_train, y_train, X_test, y_test)
--- plot.py
+++ plot.py
@@ -5,24 +5,71 @@ from sklearn import metrics
 import matplotlib.pyplot as plt
 from searcher import Searcher
 
-def plotNeighborTime( searcher, X_test, y_test, minN = 2, maxN = 10 ):
-    print('Start plotNeighborTime')
+def getNeighborFreq( searcher, X_test, y_test, minN = 2, maxN = 10 ):
+    '''
+        get Precision, Recall and frequency for one algo
+        :param searcher: Searched object
+        :param X_test: embedding for test. Vector (k,m)
+        :param y_test: corresponding class for X_test. Vector (k, )
+        :param minN: min number of nearest neighbor
+        :param maxN: max number of nearest neighbor
+        :return: precision, recall, frequency
+        '''
     n = np.arange(minN, maxN, 1)
-    times = np.zeros(maxN-minN)
+    frequency = np.zeros(maxN-minN)
+    precision = np.zeros(maxN - minN)
+    recall = np.zeros(maxN - minN)
     for n_i in n:
         print(n_i-minN)
         y_pred = np.zeros(y_test.shape)
         start  = time.time()
         for idx in range(0, len(X_test) - 1):
             y_pred[idx] = searcher.predict(X_test[idx], n=n_i)
-        times[n_i-minN] = (time.time() - start) / float(len(X_test))
-    fig_times, ax_times = plt.subplots()
-    ax_times.set(xlabel='Nearest neighbor', ylabel='Time per iter (sec)',
-           title='Time per iter / Nearest neighbor')
-    ax_times.grid()
-    ax_times.plot(n, times, 'k')
-    print('End plotNeighborTime')
-    plt.show()
+        precision[n_i - minN] = metrics.precision_score(y_test, y_pred, average='macro')
+        recall[n_i - minN] = metrics.recall_score(y_test, y_pred, average='micro')
+        frequency[n_i-minN] = float(len(X_test)) / float(time.time() - start)
+    return (precision, recall, frequency)
+
+def plotAllFreq(X_train, y_train, X_test, y_test, min_n = 2, max_n = 15):
+    '''
+    Plot Precision, Recall / frequency for all algo in algos.py/algoProd
+    :param X_train: embedding for build indices/build graph. Vector (n,m)
+    :param y_train: corresponding class for X_train. Vector (n, )
+    :param X_test:  embedding for test. Vector (k,m)
+    :param y_test:  corresponding class for X_test. Vector (k, )
+    :param min_n: min number of nearest neighbor
+    :param max_n: max number of nearest neighbor
+    :return: nothing
+    '''
+    fig_precision, ax_precision = plt.subplots()
+    ax_precision.set(xlabel='Item per second', ylabel='Precision',
+                    title='Precision / Item per second(1/sec)')
+    ax_precision.grid()
+    fig_recall, ax_recall = plt.subplots()
+    ax_recall.set(xlabel='Item per second', ylabel='Recall',
+                  title='Recall / Item per second (1/sec)')
+    ax_recall.grid()
+
+    algos = []
+    for algo in algosProd:
+        try:
+            algos.append(Searcher(X_train, y_train, algo))
+        except:
+            print('Algo init failed :(', algo)
+            pass
+
+    for searcher in algos:
+        print('Start algo test:', searcher.name)
+        (precision, recall, freq) = getNeighborFreq(searcher, X_test, y_test, min_n, max_n)
+        ax_precision.plot(freq, precision,alpha=0.6,label=searcher.name,  linewidth=2)
+        ax_recall.plot(freq, recall,alpha=0.6,label=searcher.name,  linewidth=2)
+    ax_recall.legend(loc='best')
+    ax_precision.legend(loc='best')
+
+    fig_precision.savefig('precision_time.png', dpi=600)
+    fig_recall.savefig('recall_time.png', dpi=600)
+    #plt.show()
+
 
 
 def getNeighborMetric( searcher, X_test, y_test, minN = 2, maxN = 10):
@@ -49,6 +96,8 @@ def getNeighborMetric( searcher, X_test, y_test, minN = 2, maxN = 10):
         recall[n_i-minN] = metrics.recall_score(y_test, y_pred, average='micro')
     print(searcher.name, (time.time()-start)/float(len(n)*len(X_test)))
     return(precision, recall)
+
+
 
 def plotAllMetric(X_train, y_train, X_test, y_test, min_n = 2, max_n = 15):
     '''
